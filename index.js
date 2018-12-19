7bd4b21e219a66d4d44f4e96998b3d13ef37d5d6
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-19 14:15:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ function nowServing(katzDeli){
   let i = 0;
    while (i < katzDeli.length) {
      i++;
+   }
+   if (katzDeli.length === 0) {
+     return "There is nobody waiting to be served!";
+   }
+   else
+   return (`Currently serving ${katzDeli.shift()}.`);
+ 
 }
