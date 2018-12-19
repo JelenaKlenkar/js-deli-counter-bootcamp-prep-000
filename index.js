0abdbbf5111ecc0626108dedebe58d7b5d21e90c
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-19 14:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
-function takeNumber(katzDeli,personName){
+function takeNumber(katzDeli,name){
    katzDeli.push(`${name}`);
    return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
